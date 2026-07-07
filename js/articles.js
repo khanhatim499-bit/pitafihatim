fe4cfc61ab49f7c9a06908a3aef3966780d1c352
khanhatim499-bit <khanhatim499@gmail.com>
2026-07-07 12:43:04 +0500

    Create articles.js

---
+++ js/articles.js
@@ -0,0 +1,31 @@
+const latestArticles = [
+
+{
+title: "Algebra for Beginners",
+description: "Learn variables, equations, and polynomials.",
+link: "posts/mathematics/algebra.html",
+category: "Mathematics"
+},
+
+{
+title: "Introduction to Calculus",
+description: "Study limits, continuity, derivatives, and integration.",
+link: "posts/mathematics/calculus.html",
+category: "Mathematics"
+},
+
+{
+title: "Programming Basics",
+description: "Start learning HTML, CSS, and JavaScript.",
+link: "posts/programming/index.html",
+category: "Programming"
+},
+
+{
+title: "Artificial Intelligence",
+description: "Learn AI and Machine Learning fundamentals.",
+link: "posts/ai/index.html",
+category: "Artificial Intelligence"
+}
+
+];
