a65e2b6f09a789b679f851e8ce2c00136c576fec
khanhatim499-bit <khanhatim499@gmail.com>
2026-07-09 07:19:19 +0500

    Create blog.php

---
+++ HatimsSchoolOfMathematics/blog.php
@@ -0,0 +1,75 @@
+<?php
+
+$pageTitle = "Blog";
+
+include 'includes/header.php';
+
+?>
+
+<section class="container py">
+
+    <h1 class="text-center">
+        Latest Articles
+    </h1>
+
+    <div class="blog-grid">
+
+        <article class="card">
+
+            <img src="images/default-blog.jpg" alt="Blog">
+
+            <div class="card-content">
+
+                <h3>
+                    Welcome to Hatim Education Site
+                </h3>
+
+                <p>
+                    Educational articles, technology tutorials,
+                    mathematics lessons, programming guides,
+                    and exam preparation resources will appear here.
+                </p>
+
+                <a href="#" class="btn">
+                    Read More
+                </a>
+
+            </div>
+
+        </article>
+
+
+        <article class="card">
+
+            <img src="images/default-blog.jpg" alt="Blog">
+
+            <div class="card-content">
+
+                <h3>
+                    Learning Technology
+                </h3>
+
+                <p>
+                    Explore modern technology, programming,
+                    artificial intelligence, and computer science.
+                </p>
+
+                <a href="#" class="btn">
+                    Read More
+                </a>
+
+            </div>
+
+        </article>
+
+
+    </div>
+
+</section>
+
+
+<?php
+
+include 'includes/footer.php';
+
+?>
